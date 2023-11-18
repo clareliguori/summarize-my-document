4cf9d0d723d2981a8fd849163d8fafbc442dab2c
Clare Liguori <liguori@amazon.com>
2023-11-17 16:10:39 -0800

    Add build bootstrap script to make sure fontconfig is installed

---
+++ backend/word-cloud-generator/install-fontconfig.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+is_apt_available=`command -v apt-get`
+is_yum_available=`command -v yum`
+
+if [ -n "$is_apt_available" ]; then
+    if dpkg -s fontconfig; then
+        echo "fontconfig already installed"
+        exit 0
+    fi
+
+    # Install fontconfig
+    sudo apt-get update
+    sudo apt-get -y install fontconfig
+
+elif [ -n "$is_yum_available" ]; then
+    if rpm -q fontconfig; then
+        echo "fontconfig already installed"
+        exit 0
+    fi
+
+    # Install fontconfig
+    sudo yum -y install fontconfig
+
+else
+    echo "Err: no path to apt-get or yum" >&2
+    exit 1
+fi
